a1e77e69e7d464b34978b02273302dedd67448b9
robertandrews <mail@robertandrews.co.uk>
2023-03-21 21:36:31 +0000

    Enhance appearance of thumbnails

--- post-att-overview.php
+++ post-att-overview.php
@@ -8,7 +8,6 @@ Author: Robert Andrews
 Author URI: https:/www.robertandrews.co.uk
  */
 
-
 add_action('add_meta_boxes', function () {
     add_meta_box('att_thumb_display', 'Attached Images', function ($post) {
         $args = array(
@@ -20,19 +19,28 @@ add_action('add_meta_boxes', function () {
 
         echo '<div style="display:flex;flex-wrap:wrap;">';
         foreach (get_posts($args) as $image) {
-            echo '<div style="position:relative;margin:5px;">';
-            echo '<a href="' . get_edit_post_link($image->ID) . '" target="_blank"><img src="' . wp_get_attachment_thumb_url($image->ID) . '" /></a>';
-            echo '<a href="' . wp_nonce_url(admin_url('admin-ajax.php?action=unlink_image&image_id=' . $image->ID . '&post_id=' . $post->ID . '&delete=1'), 'unlink_image_' . $image->ID) . '" style="position:absolute;top:5px;right:5px;"><i class="dashicons dashicons-trash"></i></a>';
+            $thumb_url = wp_get_attachment_image_src($image->ID, 'thumbnail', true)[0];
+            $smallest_size_url = wp_get_attachment_image_src($image->ID, 'smallest', true)[0];
+
+            echo '<div style="position:relative;margin:5px;width:150px;height:150px;overflow:hidden;">';
+            echo '<a href="' . get_edit_post_link($image->ID) . '" target="_blank"><img src="' . esc_url($thumb_url) . '" style="object-fit: cover; width: 100%; height: 100%;"/></a>';
+            echo '<a href="' . wp_nonce_url(admin_url('admin-ajax.php?action=unlink_image&image_id=' . $image->ID . '&post_id=' . $post->ID . '&delete=1'), 'unlink_image_' . $image->ID) . '" style="position:absolute;top:5px;right:5px;"><span class="dashicons dashicons-circle"><i class="dashicons dashicons-trash"></i></span></a>';
             // echo '<a href="' . get_edit_post_link($image->ID) . '" style="position:absolute;bottom:5px;right:5px;"><i class="dashicons dashicons-admin-media"></i></a>';
-            echo '<a href="' . wp_nonce_url(admin_url('admin-ajax.php?action=unlink_image&image_id=' . $image->ID . '&post_id=' . $post->ID), 'unlink_image_' . $image->ID) . '" style="position:absolute;top:5px;left:5px;"><i class="dashicons dashicons-editor-unlink"></i></a>';
+            echo '<a href="' . wp_nonce_url(admin_url('admin-ajax.php?action=unlink_image&image_id=' . $image->ID . '&post_id=' . $post->ID), 'unlink_image_' . $image->ID) . '" style="position:absolute;top:5px;left:5px;"><span class="dashicons dashicons-circle"><i class="dashicons dashicons-editor-unlink"></i></span></a>';
             echo '</div>';
         }
         echo '</div>';
-                                                            
+
         echo '<p><a href="' . admin_url('upload.php') . '?attachment&amp;post_parent=' . $post->ID . '" target="_blank"><strong>View in Media Library</strong></a></p>';
 
     }, 'post');
 });
+
+function custom_image_sizes()
+{
+    add_image_size('smallest', 150, 150, true);
+}
+add_action('after_setup_theme', 'custom_image_sizes');
 
 // Handle the unlink image action
 add_action('wp_ajax_unlink_image', function () {
@@ -60,4 +68,13 @@ add_action('wp_ajax_unlink_image', function () {
     exit();
 });
 
-?>
+function post_att_overview_enqueue_scripts()
+{
+    $screen = get_current_screen();
+    if ($screen->id !== 'post' && $screen->id !== 'edit-post') {
+        return;
+    }
+
+    wp_enqueue_style('post-att-overview-styles', plugin_dir_url(__FILE__) . 'css/styles.css', array(), '1.0.0');
+}
+add_action('admin_enqueue_scripts', 'post_att_overview_enqueue_scripts');
